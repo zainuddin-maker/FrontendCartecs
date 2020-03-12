75fb99fac89f28c6215d81826d105aa321d8619e
Riza Masta Saputra <rizamasta@gmail.com>
2020-03-12 10:17:26 +0700

    add redux

---
+++ src/redux/reducers.js
@@ -0,0 +1,20 @@
+/**
+ * Combine all reducers in this file and export the combined reducers.
+ */
+import { combineReducers } from "redux-immutable";
+import { connectRouter } from "connected-react-router/immutable";
+import { history } from "app/utils";
+
+/**
+ * Creates the main reducer with the dynamically injected ones
+ */
+export default function createReducer(injectedReducers = {}) {
+  const rootReducer = combineReducers({
+    router: connectRouter(history),
+    ...injectedReducers
+  });
+
+  // Wrap the root reducer and return a new root reducer with router state
+  const mergeWithRouterState = connectRouter(history);
+  return mergeWithRouterState(rootReducer);
+}
